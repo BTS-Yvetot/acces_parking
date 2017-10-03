498484e3ea2d93944f1d1101e77a52c59e98d449
BTS-Yvetot <sebastien.pisak@ac-rouen.fr>
2017-10-03 16:00:07 +0200

    -Modification de 12C.h afin d'utiliser la fonction initI2C(int vitesse) pour paramétrer la vitesse du bus. (typiquement vitesse=122).

--- I2C/I2C.h
+++ I2C/I2C.h
@@ -1,6 +1,5 @@
 #ifndef I2C_H
 #define I2C_H
-
 /*cette macro permet de placer a 1 le bit d'un registre*/
 #ifndef sbi
 #define sbi(sfr,bit) (_SFR_BYTE(sfr)|=_BV(bit))
@@ -10,7 +9,7 @@
 #define cbi(sfr,bit) (_SFR_BYTE(sfr)&=~_BV(bit))
 #endif
 
-
+void initI2C(int vitesse){
 
   /*division de 64 pour le prediviseur TWPS
   TWPS0=0 et TWPS1=0 ==> TWPS=0 ==> 4^TWPS=1
@@ -23,6 +22,6 @@
   489 Hz<SCL<1 000 000 Hz*/
   sbi(TWSR, TWPS0);  //reduction de la vitesse de transmission sur le
   cbi(TWSR, TWPS1);  //bus (ici 4081,63 Hz)
-  TWBR=122;
-
+  TWBR=vitesse;		//valeur comprise entre 0 et 255. Une valeur de 122 donne de bon résultats
+}
 #endif
